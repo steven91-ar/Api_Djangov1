678879de8002702dca7ab23b696a6fb77214f9f0
steven9work <72232262+steven91-ar@users.noreply.github.com>
2025-06-26 19:13:56 +0200

    commit API

---
+++ djangoapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.getUsers),
+    path('create/', views.addUser),
+    path('read/<str:pk>/', views.getUser),
+    path('update/<str:pk>/', views.updateUser),
+    path('delete/<str:pk>/', views.deleteUser),
+]
+
